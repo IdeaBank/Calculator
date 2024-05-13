50ffa4cc1aaf0e37a8361ce733d13588a04ca773
IdeaBank <hailcryptic@gmail.com>
2024-05-13 15:46:51 +0900

    fix: delete abundant code in SubtractHandler

--- src/calculation/MultiplyHandler.java
+++ src/calculation/MultiplyHandler.java
@@ -6,8 +6,6 @@ import java.math.MathContext;
 public class MultiplyHandler implements ICalculation {
     @Override
     public BigDecimal calculate(BigDecimal firstOperand, BigDecimal secondOperand) {
-        checkDivisionByZero(firstOperand, secondOperand);
-
         return firstOperand.multiply(secondOperand, MathContext.UNLIMITED);
     }
 }
